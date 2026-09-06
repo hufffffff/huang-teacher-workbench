f82cf914109d2236bc7b43cc93a0c000dbdd0a48
hufffffff <136241847+hufffffff@users.noreply.github.com>
2026-09-06 19:32:18 +0800

    升级sw缓存版本v5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='huang-workbench-v4';
+const CACHE='huang-workbench-v5';
 const ASSETS=['./黄老师工作台.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./favicon-32.png'];
 self.addEventListener('install',e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())); });
 self.addEventListener('activate',e=>{ e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
